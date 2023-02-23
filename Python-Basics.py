a0f599d0b572d5d4699473b0623929c300702d10
AlaiaS <zsunny1992@gmail.com>
2023-02-23 17:22:58 +0800

    Update Python-Basics.py

--- Python-Basics.py
+++ Python-Basics.py
@@ -78,3 +78,13 @@ print(L[2][2]) #打印Lisa
 height = int(input('Please enter your height:'))
 weight = int(input('Please enter your weight:'))
 BMI = weight / (height ** 2)
+if BMI < 18.5:
+    print('过轻')
+elif BMI < 25:
+    print('正常')
+elif BMI < 28:
+    print('过重')
+elif BMI < 32:
+    print('肥胖')
+else:
+    print('严重肥胖')
