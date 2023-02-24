1d2f089b7f22760d781854482627e91a60b5c278
AlaiaS <zsunny1992@gmail.com>
2023-02-24 17:35:24 +0800

    new file:   functions.py

---
+++ functions.py
@@ -0,0 +1,20 @@
+# %%
+a = abs(-1000)
+print(a)
+# %%
+m = max(1, 2, 3, 4, 5)
+print(m)
+# %%  
+n1 = 255
+n2 = 1000
+print(hex(n1), hex(n2))
+
+
+# %%
+def my_abs(x):
+    if x >= 0:
+        return x
+    else:
+        return -x
+print(my_abs(-99))
+# %%
