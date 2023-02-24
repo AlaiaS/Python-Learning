825d82e891efccd9ef7e9672947b83bdd91e9ffe
AlaiaS <zsunny1992@gmail.com>
2023-02-24 11:30:51 +0800

    modified:   Python-Basics.py

--- Python-Basics.py
+++ Python-Basics.py
@@ -88,3 +88,51 @@ elif BMI < 32:
     print('肥胖')
 else:
     print('严重肥胖')
+
+# %%
+sum = 0
+for x in range(101):
+    sum = sum + x
+print(sum)
+# %%
+sum = 0
+n = 99
+while n > 0:
+    sum = sum + n
+    n = n - 2
+print(sum)
+# %%
+L = ['Bart', 'Lisa', 'Adam']
+for name in L:
+    print(f'Hello, {name}!')
+# %%
+n = 1
+while n <= 100:
+    print(n)
+    n = n + 1
+print('END')
+# %%
+n = 1
+while n <= 100:
+    if n > 10:
+        break
+    print(n)
+    n = n + 1
+print('END')
+# %%
+n = 0
+while n < 10:
+    n = n + 1
+    if n % 2 == 0:
+        continue
+    print(n)
+# %%
+s = set((1,2,3))
+print(s)
+# %%
+s = set((1,[2,3]))
+print(s)
+# %%
+d = {'A':(1,2,3),'B':(1,[2,3])}
+print(d['A'])
+# %%
